b54ad5bbf68b822102d753589e98ba3dfa786c6c
Manoj <666rmanoj@gmail.com>
2023-03-17 01:53:10 +0530

    Added alembic.ini for multiple models

--- filings-backend/app/src/services/course_enq/schemas.py
+++ filings-backend/app/src/services/course_enq/schemas.py
@@ -2,8 +2,27 @@ from pydantic import BaseModel
 from datetime import date
 
 class IGS_COURSE_ENQ(BaseModel):
-   
-    req_id : str
+
+    name : str
+    followup_call_date: date
+    followup_status : str
+    enquiry_by : str
+    mobile : int
+    location : str
+    course : str
+    fee_structure : str
+    experience_by : str
+    info_source : str
+    purpose : str
+    mode : str
+    comments : str
+
+    class Config:
+        orm_mode = True
+
+class IGS_COURSE_ENQ_ID(BaseModel):
+
+    id : int
     name : str
     followup_call_date: date
     followup_status : str

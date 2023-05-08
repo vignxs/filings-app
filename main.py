7069ad74faf7fb451ea994a9c2132552d4bde614
Rahul R <109589036+rahulr001@users.noreply.github.com>
2023-05-08 22:30:13 +0530

    Update main.py

--- main.py
+++ main.py
@@ -55,7 +55,7 @@ if __name__  == "__main__":
     uvicorn.run(
             app,
             host="0.0.0.0",
-            port=8000,
+            port=5000,
             ssl_keyfile="./key.pem",
             ssl_certfile="./certificate.pem",
             )
